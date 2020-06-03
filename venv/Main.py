75f801d34b70dc3007ec3044556b266bb9a44049
d4e5 <d4e5v8@gmail.com>
2020-06-03 15:08:49 -0700

    Basic functions

--- venv/Main.py
+++ venv/Main.py
@@ -1,7 +1,39 @@
-import tiledb
+import csv
 import sys
+import os
+import configparser
+import logging
 
+# tiledb imports requirements
+import tiledb
 
+# Constants
+CONFIG_FILE = './config.ini'
+LOG_FILE = './log.txt'
 
-print(tiledb.libtiledb.version())
+# Global things
+ArrayFileName: str
 
+# Functions
+def Init_Stuff():
+    # remove the existing log file...
+    if (os.path.isfile(LOG_FILE)):
+        os.remove(LOG_FILE)
+    # start logging
+    logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG)
+    if (os.path.isfile(CONFIG_FILE)):
+        logging.info("Config file found...")
+    else:
+        logging.critical("No config file, please create one!")
+        sys.exit(1)
+    logging.info("Loading Config...")
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+    ArrayFileName: str = config['FILES']['LOCAL_ARRAY_FILE']
+    logging.info("Local Array Filename is: %s", ArrayFileName)
+
+# Main section (old school)
+print("Running... \nSee Log.txt...")
+Init_Stuff()
+logging.info("Running tileDB thing... version: %s", tiledb.libtiledb.version())
+logging.info("Current local directory: %s", os.getcwd())
